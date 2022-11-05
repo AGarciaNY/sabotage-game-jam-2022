e2f451831726a780609eb6f4482bec354d90cb6e
AGarciaNY <aldairgarcia360@gmail.com>
2022-11-05 17:27:15 -0400

    feat: added some player logic

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,10 +1,12 @@
 extends Actor
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+func _physics_process(delta: float) -> void:
+	var direction: = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		0.0
+	)
+	velocity = speed * direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
