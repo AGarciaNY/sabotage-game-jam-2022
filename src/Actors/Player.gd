72306a07250efc21ba823abf2f3dda2b0e8d4858
AGarciaNY <aldairgarcia360@gmail.com>
2022-11-05 18:38:32 -0400

    feat: more player phisics

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -2,11 +2,33 @@ extends Actor
 
 
 func _physics_process(delta: float) -> void:
-	var direction: = Vector2(
+	var is_jump_interrupted: = Input.is_action_just_pressed("jump") and velocity.y < 0.0
+	var direction: = get_direction()
+	velocity = calculate_move_velocity(velocity, speed, direction, is_jump_interrupted)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+
+
+func get_direction() -> Vector2:
+	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		0.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 	)
-	velocity = speed * direction
+
+
+func calculate_move_velocity(
+		linear_velocity: Vector2, 
+		speed: Vector2, 
+		direction: Vector2,
+		is_jump_interrupted: bool
+		) -> Vector2:
+	var new_velocity: = linear_velocity
+	new_velocity.x = speed.x * direction.x
+	new_velocity.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		new_velocity.y = speed.y * direction.y
+	if is_jump_interrupted:
+		new_velocity.y = -1.0
+	return new_velocity
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
